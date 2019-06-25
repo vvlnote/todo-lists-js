313f1c8d930618efb5c81d603839e4645fb14b83
Vivvie Lin <vvlnote@hotmail.com>
2019-06-24 22:04:33 -0700

    delete not needed codes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,28 +1,7 @@
 class Item < ApplicationRecord
-	#belongs_to :list
 	has_many :list_items
 	has_many :lists, through: :list_items
 
 	validates :description, presence: true
 
-	STATUS = {
-		:incompleted => 0,
-		:completed => 1
-	}
-
-	def completed?
-		#binding.pry
-		#self.status == STATUS[:completed]
-		false
-	end
-
-	def incompleted?
-		#binding.pry
-		#if self.status == nil || self.status == STATUS[:incompleted] 
-		#	true
-		#else
-		#	false
-		#end
-		false
-	end
 end
--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -2,7 +2,6 @@ class ListItem < ApplicationRecord
 	belongs_to :list
 	belongs_to :item
 
-	#scope :completed?, -> {where(status: 1)}
 	scope :get_list_items_of, ->(list) { where("list_id == ?", list.id)}
 
 	STATUS = {
@@ -13,9 +12,7 @@ class ListItem < ApplicationRecord
 	def self.get_items_of(list)
 		list_items = []
 		items = []
-		#list_items = self.get_list_items_of(list)
 		list_items = ListItem.get_list_items_of(list)
-		binding.pry
 		items = list_items.map {|item| Item.find(item.item_id)}
 	end
 
